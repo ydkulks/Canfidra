7b274f4132b45a0a83274d436874493ee8fb3ccb
ydkulks <ydkulks2@gmail.com>
2023-08-08 20:06:51 +0530

    removed props for icons

--- app/functional_tray.tsx
+++ app/functional_tray.tsx
@@ -2,10 +2,10 @@ import { IcRoundVideocam, IcBaselineMic } from "./icons";
 export default function FunctionalTray() {
   return (
     <div>
-      <button>
+      <button className="p2 dark:hover:text-slate-500">
         <IcRoundVideocam />
       </button>
-      <button>
+      <button className="p-2 dark:hover:text-slate-500">
         <IcBaselineMic />
       </button>
     </div>
