476fb945a5db76b7855cd8906cf4a349e9ea4b61
ydkulks <ydkulks2@gmail.com>
2023-08-20 17:14:16 +0530

    Added baseline chat feature
    Chat feature with unpolished UI and functionality

--- app/icons.tsx
+++ app/icons.tsx
@@ -1,6 +1,6 @@
 export function GalaVideo() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 256 256" ><g id="galaVideo0" fill="none" stroke="currentColor" strokeDasharray="none" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="4" strokeOpacity="1" strokeWidth="16"><path id="galaVideo1" d="M 47.999992,48 H 160 c 17.728,0 32,14.272 32,32 v 96 c 0,17.728 -14.272,32 -32,32 H 47.999992 c -17.728,0 -32,-14.272 -32,-32 V 80 c 0,-17.728 14.272,-32 32,-32 z"></path><path id="galaVideo2" d="m 192,160 48,32 V 64 l -48,32"></path></g></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 256 256" ><g id="galaVideo0" fill="none" stroke="currentColor" strokeDasharray="none" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="4" strokeOpacity="1" strokeWidth="16"><path id="galaVideo1" d="M 47.999992,48 H 160 c 17.728,0 32,14.272 32,32 v 96 c 0,17.728 -14.272,32 -32,32 H 47.999992 c -17.728,0 -32,-14.272 -32,-32 V 80 c 0,-17.728 14.272,-32 32,-32 z"></path><path id="galaVideo2" d="m 192,160 48,32 V 64 l -48,32"></path></g></svg>
   )
 }
 
@@ -20,5 +20,31 @@ export function IcRoundVideocam() {
 export function IcRoundScreenShare() {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 24 24" ><path fill="currentColor" d="M20 18c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2v10a2 2 0 0 0 2 2H1c-.55 0-1 .45-1 1s.45 1 1 1h22c.55 0 1-.45 1-1s-.45-1-1-1h-3zm-7-3.53v-2.19c-2.78 0-4.61.85-6 2.72c.56-2.67 2.11-5.33 6-5.87V7l3.61 3.36c.21.2.21.53 0 .73L13 14.47z"></path></svg>
+  )
+}
+
+
+export function IcRoundSend() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 24 24" ><path fill="currentColor" d="m3.4 20.4l17.45-7.48a1 1 0 0 0 0-1.84L3.4 3.6a.993.993 0 0 0-1.39.91L2 9.12c0 .5.37.93.87.99L17 12L2.87 13.88c-.5.07-.87.5-.87 1l.01 4.61c0 .71.73 1.2 1.39.91z"></path></svg>
+  )
+}
+
+
+export function IcRoundPhoneDisabled() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 24 24" ><path fill="currentColor" d="M14.54 17.37a16.868 16.868 0 0 1-9.45 3.61c-1.13.07-2.07-.87-2.07-2v-1.73c-.01-1.01.75-1.86 1.76-1.98l2.54-.29c.61-.07 1.21.14 1.64.57l1.84 1.84c.81-.41 1.59-.9 2.31-1.45L2.1 4.93a.996.996 0 0 1 0-1.41c.39-.39 1.03-.39 1.42 0L20.49 20.5a.996.996 0 1 1-1.41 1.41l-4.54-4.54zm2.85-6.57l-1.85-1.85a1.99 1.99 0 0 1-.57-1.64l.29-2.52c.12-1.01.97-1.77 1.99-1.77h1.73c1.13 0 2.07.94 2 2.07a16.94 16.94 0 0 1-3.62 9.47l-1.43-1.43c.55-.73 1.04-1.51 1.46-2.33z"></path></svg>
+  )
+}
+
+export function IcRoundMessage() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 24 24" ><path fill="currentColor" d="M20 2H4c-1.1 0-1.99.9-1.99 2L2 22l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-3 12H7c-.55 0-1-.45-1-1s.45-1 1-1h10c.55 0 1 .45 1 1s-.45 1-1 1zm0-3H7c-.55 0-1-.45-1-1s.45-1 1-1h10c.55 0 1 .45 1 1s-.45 1-1 1zm0-3H7c-.55 0-1-.45-1-1s.45-1 1-1h10c.55 0 1 .45 1 1s-.45 1-1 1z"></path></svg>
+  )
+}
+
+export function IcRoundAccountCircle() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 24 24" ><path fill="currentColor" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2zm0 4c1.93 0 3.5 1.57 3.5 3.5S13.93 13 12 13s-3.5-1.57-3.5-3.5S10.07 6 12 6zm0 14c-2.03 0-4.43-.82-6.14-2.88a9.947 9.947 0 0 1 12.28 0C16.43 19.18 14.03 20 12 20z"></path></svg>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,21 +3,101 @@ import Navbar from "./navbar";
 import Camera from "./camera";
 import Microphone from "./microphone";
 import Screen_Share from "./screen_share";
-import { IcRoundVideocam, IcBaselineMic, IcRoundScreenShare } from "./icons";
+import {
+  IcRoundVideocam,
+  IcBaselineMic,
+  IcRoundScreenShare,
+  IcRoundSend,
+  IcRoundMessage,
+  IcRoundPhoneDisabled,
+  IcRoundAccountCircle,
+} from "./icons";
 import { useState } from "react";
+
+interface MessageProps {
+  message: boolean;
+}
+const Chat: React.FC<MessageProps> = ({ message }) => {
+  const [inputValue, setInputValue] = useState("");
+  const [submittedValue, setSubmittedValue] = useState("");
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+  const handleSubmit = (event: React.ChangeEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const trimmedValue = inputValue.trim();
+    if (trimmedValue !== "") {
+      setSubmittedValue(inputValue);
+      setInputValue("");
+    }
+  };
+
+  return (
+    <div>
+      {message ? (
+        <div className="absolute right-0 left-auto top-0 h-full">
+          <div className="bg-gray-800 min-w-10 max-w-20 min-h-full p-5">
+            <div className="">
+              {submittedValue && (
+                <div>
+                  <IcRoundAccountCircle />
+                  <p className="dark:text-slate-400">User name</p>
+                  <div className="dark:bg-slate-600 w-fit px-3 py-1 rounded-md">
+                    <p>{submittedValue}</p>
+                  </div>
+                </div>
+              )}
+            </div>
+            <form onSubmit={handleSubmit} className="flex my-2">
+              <input
+                className="dark:bg-slate-600 rounded-l-md h-10 p-3 ml-1 items-center"
+                type="text"
+                placeholder="Send a message"
+                value={inputValue}
+                onChange={handleInputChange}
+              />
+              <button
+                className="text-green-500 bg-slate-600 hover:text-green-600 mr-1 rounded-r-md pr-2 pl-2"
+                type="submit"
+              >
+                <IcRoundSend />
+              </button>
+            </form>
+          </div>
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
 export default function Home() {
+  const [account, setAccount] = useState(false);
   const [screenShare, setScreenShare] = useState(false);
   const [mic, setMic] = useState(false);
   const [camera, setCamera] = useState(false);
+  const [callState, setCallState] = useState(false);
+  const [message, setMessage] = useState(false);
   return (
     <main className="min-h-screen bg-slate-900">
       <Navbar />
+      <Chat message={message} />
       <Camera camera={camera} />
       <Microphone mic={mic} />
       <Screen_Share screenShare={screenShare} />
 
-			{/* Buttons for Video, Audio and Screen share*/}
-      <div className="absolute w-full bottom-0 flex justify-center">
+      {/* Buttons for Video, Audio and Screen share*/}
+      <div className="absolute w-full bottom-0 flex justify-between">
+        <div>
+          <button
+            className={`m-2 p-2 rounded-full hover:dark:bg-slate-700 ${
+              account ? "bg-red-500" : ""
+            }`}
+            onClick={() => setAccount(!account)}
+          >
+            <IcRoundAccountCircle />
+          </button>
+        </div>
+        <div>
           <button
             className={`m-2 p-2 rounded-full hover:dark:bg-slate-700 ${
               camera ? "bg-red-500" : ""
@@ -42,7 +122,24 @@ export default function Home() {
           >
             <IcRoundScreenShare />
           </button>
+          <button
+            className="m-2 p-2 rounded-full bg-red-500 hover:bg-red-600"
+            onClick={() => setCallState(!callState)}
+          >
+            <IcRoundPhoneDisabled />
+          </button>
         </div>
+        <div className="items-end">
+          <button
+            className={`m-2 p-2 rounded-full hover:dark:bg-slate-700 ${
+              message ? "bg-red-500" : ""
+            }`}
+            onClick={() => setMessage(!message)}
+          >
+            <IcRoundMessage />
+          </button>
+        </div>
+      </div>
     </main>
   );
 }
